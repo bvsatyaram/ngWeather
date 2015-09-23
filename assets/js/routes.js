5ce43fd8c8bfaf2208237372a238f02429896b18
BV Satyaram <bvsatyaram@gmail.com>
2015-09-24 03:25:01 +0530

    Trying basepath for github pages

--- assets/js/routes.js
+++ assets/js/routes.js
@@ -1,17 +1,21 @@
+// weatherApp.config(function ($locationProvider) {
+//   $locationProvider.html5Mode(true);
+// });
+
 weatherApp.config(function($routeProvider) {
   $routeProvider
     .when('/', {
-      templateUrl: './../../pages/home.html',
+      templateUrl: '../../pages/home.html',
       controller: 'homeCtrl',
       controllerAs: 'home'
     })
     .when('/forecast', {
-      templateUrl: './../../pages/forecast.html',
+      templateUrl: '../../pages/forecast.html',
       controller: 'forecastCtrl',
       controllerAs: 'forecast'
     })
     .when('/forecast/:days', {
-      templateUrl: './../../pages/forecast.html',
+      templateUrl: '../../pages/forecast.html',
       controller: 'forecastCtrl',
       controllerAs: 'forecast'
     })
